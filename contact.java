2a35285fbe1690596cac58db262f380d2b636122
Saurav Maan <souravmaan1@gmail.com>
2020-07-25 23:30:57 +0530

    Add files via upload

---
+++ contact.java
@@ -0,0 +1,59 @@
+package com.example.android.mcdonalds;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class contact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);    }
+    public void dial(View view)
+    {
+        String number="01166000666";
+
+        Intent intent=new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + number));
+        if(intent.resolveActivity(getPackageManager())!=null)
+        {
+            startActivity(intent);
+        }
+
+
+    }
+    public void email(View view)
+    {
+        String[] address={"info@mcdonaldsindia.com"};
+
+        Intent intent=new Intent(Intent.ACTION_SENDTO);
+
+        intent.setData(Uri.parse("mailto:"));
+
+        intent.putExtra(Intent.EXTRA_EMAIL, address);
+        if(intent.resolveActivity(getPackageManager())!=null)
+        {
+            startActivity(intent);
+        }
+
+
+    }
+    public void home(View view)
+    {
+        Intent intent;
+
+
+        intent = new Intent(this, mainPage.class);
+
+
+        startActivity(intent);
+
+    }
+
+}
